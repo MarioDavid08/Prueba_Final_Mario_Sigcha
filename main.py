30203dcfc92583040b4cae7f536c04bea2034acc
MarioDavid08 <mariodav20@gmail.com>
2023-09-28 22:11:12 -0500

    Segundo Commit

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from mongo import MongoConnection
+
+
+db_client = MongoConnection().client
+db = db_client.get_database('clasificacion')
+col = db.get_collection('pilots_panel_f1')
 
 
 driver = webdriver.Chrome()
@@ -13,7 +19,14 @@ for f in pilots_panel_f1:
     print(" ",punt_name, "puntos")
     print('-' * 20)
 
+    document = {
+        "posición pilotos": pos_pilots_name,
+        "puntos": punt_name,
+    }
+    col.insert_one(document=document)
 
+
+col = db.get_collection('pilots_panel_moto_GP')
 driver = webdriver.Chrome()
 driver.get("https://resultados.as.com/resultados/motor/motogp/clasificacion/")
 pilots_panel_gp = driver.find_elements(By.CLASS_NAME, "row-table-datos")
@@ -24,6 +37,13 @@ for f in pilots_panel_gp:
     punt_name = f.find_element(by=By.CLASS_NAME, value="destacado").accessible_name
     print(" ",punt_name, "puntos")
     print('-' * 20)
+
+    document = {
+        "posición pilotos": pos_pilots_name,
+        "puntos": punt_name,
+    }
+
+    col.insert_one(document=document)
 
 
 driver.close()
